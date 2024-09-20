110aab0a6bc74fa64c636e25d7e2115c1d2cce9e
fzj0007 <42284369+fzj0007@users.noreply.github.com>
2024-09-19 22:58:52 -0700

    New update

--- ObstacleCourse/Assets/Scripts/ObjectHit.cs
+++ ObstacleCourse/Assets/Scripts/ObjectHit.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class ObjectHit : MonoBehaviour
 {
-   
+    private void OnCollisionEnter(Collision other) 
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            GetComponent<MeshRenderer>().material.color = Color.red;
+            gameObject.tag = "Hit";
+        }
+    }
 }
